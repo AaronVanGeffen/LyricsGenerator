19f93e046f9806b857859f936ba36ab6ff32209f
Aaron van Geffen <aaron@aaronweb.net>
2017-04-22 00:38:29 +0200

    Compact language detection script output.

--- detect_languages.py
+++ detect_languages.py
@@ -4,11 +4,11 @@ from langdetect import detect
 import os
 
 for root, dirs, files in os.walk("./results/"):
-	path = root.split(os.sep)
-	print((len(path) - 1) * '-', os.path.basename(root))
-
 	for file in files:
+		if file == "genre.txt":
+			continue
+
 		with open(os.path.join(root, file), 'r') as handle:
 			lyrics = handle.read()
 			lang = detect(lyrics)
-			print(len(path) * '-', file, '->', lang)
+			print(os.path.join(root, file), '->', lang)
