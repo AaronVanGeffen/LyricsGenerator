119d271e2c1f4ec71dbac932526e64ca4191f768
johannadevos <johannadevos@gmail.com>
2017-04-24 15:20:06 +0200

    Added tokens to mark start and end of lyric

--- lyricsparser.py
+++ lyricsparser.py
@@ -7,9 +7,19 @@ pd.set_option('display.width', 120)
 
 # Read CSV into data frame.
 df = pd.read_csv("lyrics_eng.csv", header = None, names = ['artist', 'year', 'title', 'genre', 'lyrics'])
+df2 = df.ix[0:1000]
 
 # Example: get row 5.
 row = df.ix[5]
 
 # Example: print genre from row above.
 print(row['genre'])
+
+# Add start and end token of lyric.
+df.loc[:,'lyrics'] = "<S>" + df['lyrics'] + "<E>"
+    
+# Print lyric.
+df.ix[0]['lyrics']
+
+# Write to CSV.
+df2.to_csv("lyrics_start_end.csv")   
