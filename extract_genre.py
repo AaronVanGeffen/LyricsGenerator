8789887997ef1d1f04ce3cec25b8ac169418f1cf
Johanna de Vos <johannadevos@gmail.com>
2017-05-12 13:56:53 +0200

    Export multiple genres through export script.

--- extract_genre.py
+++ extract_genre.py
@@ -1,22 +1,32 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 import pandas as pd
 import numpy as np
 
-genre = "Rock"
+def readData():
+	# Read CSV into data frame.
+	df = pd.read_csv("lyrics/lyrics_eng_skipped_5pct_non-ascii.csv", header = None, names = ['artist', 'year', 'title', 'genre', 'lyrics'])
+	print(df.shape)
+	return df
 
-pd.set_option('display.width', 120)
+def exportGenre(df, genre):
+	print ("Now exporting ", genre)
+	pd.set_option('display.width', 120)
 
-# Read CSV into data frame.
-df = pd.read_csv("lyrics_eng_skipped_5pct_non-ascii.csv", header = None, names = ['artist', 'year', 'title', 'genre', 'lyrics'])
-print(df.shape)
-df_genre = df[df['genre'] == genre]
-print(df_genre.shape)
+	df_genre = df[df['genre'] == genre]
+	print(df_genre.shape)
 
-df_sample = df_genre.ix[np.random.choice(df_genre.index, 10000, replace=False)]
-#print(df_sample)
-print(df_sample.shape)
+	df_sample = df_genre.ix[np.random.choice(df_genre.index, 10000, replace=False)]
+	#print(df_sample)
+	print(df_sample.shape)
 
-with open("lyrics/" + genre + ".txt", "a") as f:
-	for index, row in df_sample.iterrows():
- 			f.write("<S>\n" + row['lyrics'] + "\n<E>\n")
+	with open("lyrics/" + genre + ".txt", "a") as f:
+		for index, row in df_sample.iterrows():
+				f.write("<S>\n" + row['lyrics'] + "\n<E>\n")
+
+df = readData()
+
+genres = ["Country", "Metal", "Pop"]
+for genre in genres:
+	exportGenre(df, genre)
+
