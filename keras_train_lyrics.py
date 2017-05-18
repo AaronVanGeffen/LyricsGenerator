5b1d5002ffccb89650304cbf3290ae7e72ef2d9c
Aaron van Geffen <aaron@aaronweb.net>
2017-05-18 17:29:58 +0200

    Add extra layer to RNN model; save worse-performing epochs too.

--- keras_train_lyrics.py
+++ keras_train_lyrics.py
@@ -53,7 +53,9 @@ y = np_utils.to_categorical(dataY)
 
 # define the LSTM model
 model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
@@ -64,7 +66,7 @@ if not os.path.isdir(args.outdir):
 
 # define the checkpoint
 filepath = os.path.join(args.outdir, "weights-improvement-{epoch:02d}-{loss:.4f}.hdf5")
-checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=False, mode='min')
 callbacks_list = [checkpoint]
 
-model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
+model.fit(X, y, epochs=50, batch_size=128, callbacks=callbacks_list)
